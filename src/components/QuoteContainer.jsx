eba4273f46232802ad79ef2dfd597735b81b6690
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-09-28 11:09:35 -0600

    click handler on button to update state

--- src/components/QuoteContainer.jsx
+++ src/components/QuoteContainer.jsx
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import Quote from './Quote';
 
 class QuoteContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quote: 'quotie boi' };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState(state => ({
+      quote: 'random quotie boi'
+    }));
+  }
+
   render() {
     return (
       <>
-        <Quote quote='cool quote'/>
-        <button>Inspire me</button>
+        <Quote quote={this.state.quote} />
+        <button className="button" onClick={this.handleClick} >Inspire me</button>
       </>
     );
   }
