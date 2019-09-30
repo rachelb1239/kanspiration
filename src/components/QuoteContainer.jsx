63a6ed9fafe9aeb6803ff8723f66cace8e925e6d
rachelb1239 <rachel@Rachels-MacBook-Pro.local>
2019-09-29 20:48:26 -0600

    stylish memories

--- src/components/QuoteContainer.jsx
+++ src/components/QuoteContainer.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Quote from './Quote';
+import Memories from './Memories';
 
 class QuoteContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { quote: 'quotie boi' };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      quote: 'Not much inspiration here...',
+      memories: []
+   };
+    this.handleGetQuoteClick = this.handleGetQuoteClick.bind(this);
+    this.handleSaveMemoryClick = this.handleSaveMemoryClick.bind(this);
+    this.handleClearMemoryClick = this.handleClearMemoryClick.bind(this);
     this.fetchQuote = this.fetchQuote.bind(this);
   }
 
@@ -15,7 +21,7 @@ class QuoteContainer extends Component {
     return quote;
   }
 
-  handleClick() {
+  handleGetQuoteClick() {
     this.fetchQuote()
     .then((response) => {
       this.setState(state => ({
@@ -25,11 +31,37 @@ class QuoteContainer extends Component {
     .catch((error) => console.log(error))
   }
 
+  handleSaveMemoryClick() {
+    const { quote, memories } = this.state;
+    if (!memories.includes(quote)) {
+      const tempMemories = memories.slice(0);
+      tempMemories.push(quote);
+      this.setState(state => ({
+        memories: tempMemories
+      }));
+    }
+  }
+
+  handleClearMemoryClick() {
+    this.setState(state => ({
+      memories: []
+    }));
+  }
+
   render() {
     return (
       <>
+      <div className="container">
         <Quote quote={this.state.quote} />
-        <button className="button" onClick={this.handleClick} >Inspire me</button>
+      </div>
+      <div className="container">
+        <button className="button" onClick={this.handleGetQuoteClick} >Inspire me</button>
+        <button className="button" onClick={this.handleSaveMemoryClick} >Save my Inspiration</button>
+        <button className="button" onClick={this.handleClearMemoryClick} >Wipe my Memory</button>
+      </div>
+      <div className="container">
+        <Memories memories={this.state.memories} />
+      </div>
       </>
     );
   }
